0a1b0a58ae80480f02f5901dbc5e230f017a43c2
Nehallearn <learn5151@gmail.com>
2020-02-23 11:38:27 +0000

    23rd Feb First Commit

--- pages/home.page.js
+++ pages/home.page.js
@@ -9,6 +9,19 @@ class Homepage{
     get clickOnloginbuton(){
         return $('//input[@type="submit"][@value="Login"]');
     }
+
+     usrnameValue(text1){
+        this.usrname.waitForDisplayed();
+        this.usrname.setValue(text1);
+    }
+     pwdValue(text2){
+        this.pwd.waitForDisplayed();
+        this.pwd.setValue(text2);
+    }
+     clickonLogin(){
+        this.clickOnloginbuton.waitForDisplayed(); 
+        this.clickOnloginbuton.click();
+    }
 }
 
 module.exports = new Homepage();
--- test/index.js
+++ test/index.js
@@ -1,19 +1,41 @@
 const homePage1 = require("../pages/home.page")
 
-describe.skip("First test", function()
+describe("First test", function()
 {
-  it.skip("First it bock", function(){
+  it("First it bock", function(){
 
-      browser.url('/');
+          browser.url('/');
      
-         homePage1.usrname.setValue('nehal_51');
-         homePage1.pwd.setValue('India786');
-         homePage1.clickOnloginbuton.click();
-         browser.pause(5000);
-   
+         //homePage1.usrname.setValue('nehal_51');
+         //homePage1.pwd.setValue('India786');
+         //homePage1.clickOnloginbuton.click();
+         homePage1.usrnameValue("nehal_51");
+         assert.equal('nehal_51', homePage1.usrname.getValue());
+        
+  })
+
+  it('second it block', function(){
+    
+    homePage1.pwdValue("India786");
+    assert.equal('India786',homePage1.pwd.getValue());
+
 
 
   })
 
+ 
+  it('Fourth it block', function(){
 
+      homePage1.usrname.click();
+      homePage1.usrname.clearValue();
+      assert.equal('', homePage1.usrname.getValue());
+        
+  })
+  it('Third it block', function(){
+    
+    homePage1.clickonLogin();
+    browser.pause('5000');
+     
+
+  })
 })
